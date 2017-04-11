df0df2e6dafc0d111bb4c84e42266def7ae086d4
joeListro <jlistro@gmail.com>
2017-04-11 11:28:48 -0400

    initial commit

---
+++ BleepBot/source.cpp
@@ -0,0 +1,17 @@
+/* ***  Author:  Joe Listro and Sam Poquette
+*  Last Update:  April 11, 2017
+*     Filename:  source.cpp
+*  Description:  Accepts input in the form of a text file and replaces any ‘bad’ words then saves to a file and outputs the result to a console.
+*
+*  Certification of Authenticity:
+*     I certify that this assignment is entirely my own work.
+**********************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
